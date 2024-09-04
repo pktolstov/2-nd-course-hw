260ad4ae3f1936730129e5eee67b88e164b9f003
pktolstov <pktolstov@gmail.com>
2024-09-04 23:53:22 +0300

    JS HW2 file and mobile site version

---
+++ hw2.js
@@ -0,0 +1,90 @@
+//task1
+
+let a = 10;
+alert(a);
+console.log(a);
+a = 20;
+alert(a)
+console.log(a);
+
+
+//task2
+const iPhoneFirstRelease = "09.01.2007";
+alert(iPhoneFirstRelease);
+
+//task3
+const javaScriptOwner = "Брендан Эйх";
+alert(javaScriptOwner);
+console.log(javaScriptOwner);
+
+//task4
+let firstNumber = 10;
+let secondNumber = 2;
+alert(10 + 2);
+console.log(10 + 2);
+alert(10 - 2);
+console.log(10 - 2);
+alert(10 * 2);
+console.log(10 * 2);
+alert(10 / 2);
+console.log(10 / 2);
+
+
+//task5
+let result = 2 ** 5;
+alert(result);
+console.log(result);
+
+//task6
+let aa = 9;
+let bb = 2;
+alert(aa % bb);
+console.log(aa % bb);
+
+//task7
+let num = 1;
+num += 5;
+num -= 3;
+num *= 7;
+num /= 3;
+num++;
+num--;
+alert(num);
+console.log(num);
+
+//task8
+let age = prompt("Сколько вам лет?");
+alert(age);
+console.log(age);
+
+//task9
+const user = {
+    name: 'MrPaul',
+    age: 49,
+    isAdmin: true
+}
+console.log(user);
+
+//task10
+let name = prompt('Как твое имя?');
+alert(`Привет, ${name.toUpperCase()}!`);
+
+
+//task11
+let number = Number(prompt("Введите число"));
+let doubleNumber = number * 2;
+alert(doubleNumber);
+console.log(doubleNumber);
+let thirdNumber = doubleNumber + 10;
+alert(thirdNumber);
+console.log(thirdNumber);
+let nextNumber = thirdNumber / 2;
+alert(nextNumber);
+console.log(nextNumber);
+
+let finalNumber = nextNumber - number;
+alert(finalNumber);
+console.log(finalNumber);
+
+alert('Ответ равен 5');
+console.log('Ответ равен 5');
